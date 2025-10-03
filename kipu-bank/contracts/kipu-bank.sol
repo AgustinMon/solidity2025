72add676fbbc69d17e964a0346796c4508269cb7
agustin mondelli <agustinmondelli@hotmail.com>
2025-10-03 11:37:28 -0300

    modificaciones para cumplir con la consigna

--- kipu-bank/contracts/kipu-bank.sol
+++ kipu-bank/contracts/kipu-bank.sol
@@ -1,16 +1,28 @@
-//SPDX-License-Identifier: UNLICENSED
-pragma solidity >=0.8.0; // Solidity 0.8.0+ revierte overflow/underflow automáticamente.
+/// SPDX-License-Identifier: UNLICENSED
+pragma solidity >=0.8.0; /// Solidity 0.8.0+ revierte overflow/underflow automáticamente.
 
+
+/// @title KipuBank - Un banco simple para depósitos y retiros de ETH
+/// @author Agustín M.
+/// @notice Este contrato permite a los usuarios depositar y retirar fondos.
+/// @notice Depósito mínimo 0.01 ether.
+/// @notice Límite máximo de retiro por transacción: definido en el deploy.
+/// @dev Incluye funciones especiales del owner que permiten al dueño devolver fondos a usuarios.
 contract KipuBank {
 
-    address immutable public owner; //public para generar transparencia
-    uint256 public constant MINIMUMAMOUNT = 0.01 ether;
+    address immutable public owner; /// public para generar transparencia
+    uint256 immutable MAXIMUMTOWITHDRAW; /// cantidad maxima para retirar en una sola transaccion
+    uint256 immutable BANKCAPLIMIT; /// cantidad maxima global de depositos en el contrato
+    uint256 public constant MINIMUMDEPOSITAMOUNT = 0.01 ether; /// cantidad minima para depositar
+    uint256 public totalDeposits = 0; /// variable para llevar el control de los depositos globales
+    uint256 public totalWithdraws = 0; /// variable para llevar el control de los retiros globales
 
     mapping(address=>uint256) public balance; 
     
     error InvalidMinimum(string errorMessage);
     error Unauthorized(string errorMessage);
     error InvalidAmount(string errorMessage);
+    error ExceededGlobalLimit(string errorMessage);
 
     event Deposited(address indexed  payer, uint256 amount);
     event WithDrawn(address indexed withdrawer, uint256 amount);
@@ -20,79 +32,114 @@ contract KipuBank {
         _;
     }
 
-    constructor() {
-        owner = msg.sender; //el valor de owner queda determinado en el deploy
+    modifier VerifyBankCapLimit() {
+        if (address(this).balance > BANKCAPLIMIT) revert ExceededGlobalLimit("Exceeded global deposit limit.");
+        _;
     }
 
-    function addAmount() external payable {
-        /* 
-            Add ether to your balance, only if the amount is greater 0.1 ether 
-            Agrega dinero a tu balance, solo si la cantidad es mayor a 0.1 ether
-        */
-        if (msg.value < MINIMUMAMOUNT) revert InvalidMinimum("You must send at least 0.01 ether");
+    modifier VerifyMinimumDeposit() {
+        if (msg.value < MINIMUMDEPOSITAMOUNT) revert InvalidMinimum("You must send at least 0.01 ether");
+        _;
+    }
+
+    /// @dev depoistis limits are set at deployment
+    constructor(uint256 _globalDepositLimit) {
+        owner = msg.sender; 
+        MAXIMUMTOWITHDRAW = 0.01 ether; 
+        BANKCAPLIMIT = _globalDepositLimit; 
+    }
+
+    /**
+     * @notice Add ether to your balance, only if the amount is greater 0.1 ether 
+     * @notice Agrega dinero a tu balance, solo si la cantidad es mayor a 0.1 ether
+     */
+    function addAmount() external payable VerifyBankCapLimit VerifyMinimumDeposit{
         balance[msg.sender] += msg.value;
-        emit Deposited(msg.sender, msg.value); //evento para web3
+        emit Deposited(msg.sender, msg.value); /// evento para web3
     }
 
+    /** 
+    *  @notice Funcion para que el msg.sender pueda retirar una cantidad parcial
+    *  @notice Function for the msg.sender to withdraw a partial amount
+    */
     function withdrawPartialUsers(uint256 _amount) external returns (bytes memory) {
-        /* 
-            Funcion para que el msg.sender pueda retirar una cantidad parcial
-            Function for the msg.sender to withdraw a partial amount
-        */
         uint256 amount = _amount;
         uint256 userBalance = balance[msg.sender];
-        //chequeo balance > amount en _substractBalance
+        /// chequeo balance > amount en _substractBalance
         balance[msg.sender] = _substractBalance(userBalance,amount);
-        // prevenido contra reentrancy attack
+        /// prevenido contra reentrancy attack
         (bool success, bytes memory data) = payable(msg.sender).call{value: amount}("");
         if (!success) {
             revert();
         }
-        emit WithDrawn(msg.sender, amount); //evento para web3
+        ++totalWithdraws;
+        emit WithDrawn(msg.sender, amount); /// evento para web3
         return data;
     }
 
+     /**
+     * @notice retorna el balance de la cuenta que està usando el contrato.
+     * @notice returns the balance of the account that is using the contract.
+     * @return uint256 = balance de la cuenta
+     */
     function getBalance() external view returns(uint256) {
         return balance[msg.sender];
     }
 
+     /**
+     * @notice retorna cantidad total de depositos realizados por usuarios. No considera los depositos realizados por el owner.
+     * @notice returns total number of deposits made by users. Does not consider deposits made by the owner
+     */
+    function getTotalDeposits() external view returns(uint256) {
+        return totalDeposits;
+    }
+
+    /**
+    * @notice Retorna cantidad total de retiros realizados por usuarios. No considera los retiros realizados por el owner
+    * Returns total number of withdraws made by users. Does not consider withdrawals made by the owner
+    */
+    function getTotalWithdraws() external view returns(uint256) {
+        
+        return totalWithdraws;
+    }
+
+    /**  
+    * @notice Funcion para obtener el valor total del contrato y agregar transparencia publica.
+    * @notice Function to get the total value of the contract and add public transparency.
+    */
     function getTotalAllocated() external view returns(uint256) {
-        /* 
-            Function to get the total balance of the contract
-            Funcion para obtener el valor total del contrato 
-            Funcion para agregar transparencia publica
-        */
         return address(this).balance; 
     }
 
-    //FUNCIONES SOLO PARA EL OWNER DEL CONTRATO
+    /**
+     * @dev FUNCIONES SOLO PARA EL OWNER DEL CONTRATO
+     * Funcion para que el owner pueda devolver todo el balance de un contrato de terceros
+     * Function for the owner to withdraw and send all it balance to a third party address
+     * @param _anyAddrress = direccion del contrato de terceros
+     */
     function withdrawAll(address _anyAddrress) external onlyOwner returns(bytes memory) {
-        /* 
-            Funcion para que el owner pueda devolver todo el balance de un contrato de terceros
-            Function for the owner to withdraw and send all it balance to a third party address
-            arguments: _anyAddrress = direccion del contrato de terceros
-        */
-        address to = _anyAddrress;
+         address to = _anyAddrress;
         uint256 userBalance = balance[_anyAddrress];
         balance[_anyAddrress] = _substractBalance(userBalance, userBalance);
-        // prevenido contra reentrancy attack
+        /// prevenido contra reentrancy attack
         (bool success, bytes memory data) = to.call{value: userBalance}("");
         if(!success) revert();
-        emit WithDrawn(msg.sender, userBalance); //evento para web3
+        emit WithDrawn(msg.sender, userBalance); /// evento para web3
         return data;
     }
 
+    /**
+    * @dev FUNCIONES SOLO PARA EL OWNER DEL CONTRATO
+    * @notice Funcion para que el owner pueda devolver una cantidad parcial de un contrato a terceros
+    * @notice Function for the owner to withdraw a partial it amount to a third party address
+    * @param _anyAddress = direccion del contrato de terceros
+    * @param _amount = cantidad a retirar
+    */
     function withdrawPartialFromOwner(address _anyAddress, uint256 _amount) external onlyOwner returns (bytes memory) {
-        /* 
-            Funcion para que el owner pueda devolver una cantidad parcial de un contrato a terceros
-            Function for the owner to withdraw a partial it amount to a third party address
-            arguments: _anyAddress = direccion del contrato de terceros
-                       _amount = cantidad a retirar
-        */
         uint256 userBalance = balance[_anyAddress];
-        //chequeo balance > amount en _substractBalance
+        /// chequeo balance > amount en _substractBalance
         balance[_anyAddress] = _substractBalance(userBalance, _amount);
-        // prevenido contra reentrancy attack
+        /// prevenido contra reentrancy attack
         (bool success, bytes memory data) = payable(_anyAddress).call{value: _amount}("");
         if (!success) {
             revert();
@@ -101,18 +148,20 @@ contract KipuBank {
         return data;
     }
 
-    //FUNCIONES PRIVADAS
-    function _substractBalance(uint256 _actualBalance, uint256 _amountToReduce) private pure returns (uint256) {
-        /*
-            Reduce el balanace de una direccion en una cantidad determinada
-            Independientemente de que consume un poquito más gas, se decidió crear esta función para separar trabajos.
-            Reduces the balance of an address by a determined amount
-            Regardless of whether more gas is consumed, it was decided to create this function to separate tasks
-            arguments: _actualBalance = balance actual de la direccion
-                       _amountToReduce = cantidad a reducir
-        */
+    /**
+    * @notice Reduce el balanace de una direccion en una cantidad determinada
+    * @notice Independientemente de que consume un poquito más gas, se decidió crear esta función para separar trabajos.
+    * @notice Reduces the balance of an address by a determined amount
+    * @notice Regardless of whether more gas is consumed, it was decided to create this function to separate tasks
+    * @param _actualBalance = balance actual de la direccion
+    * @param _amountToReduce = cantidad a reducir
+    * @return uint256 = retorna el balance actualizado
+    * @dev Solo el owner puede retirar más de MAXIMUMTOWITHDRAW
+    */
+    function _substractBalance(uint256 _actualBalance, uint256 _amountToReduce) private view returns (uint256) {
         if (_actualBalance == 0) revert InvalidAmount("No balance to withdraw");
         if (_amountToReduce > _actualBalance) revert InvalidAmount("Invalid amount to withdraw");
+        if (_amountToReduce > MAXIMUMTOWITHDRAW && msg.sender != owner) revert InvalidAmount("Invalid amount to withdraw.");
         return _actualBalance - _amountToReduce;
     }
 }
